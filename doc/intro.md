78f83313bcb5316d39011f3e4a6247d491c394e2
danorel <mail.ordan@gmail.com>
2023-02-12 14:18:10 +0200

    Add initial setup for Leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clodictearch 
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clodictearch.clj
@@ -0,0 +1,9 @@
+(ns clodictearch
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(-main)
